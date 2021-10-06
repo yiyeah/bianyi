88f7c9fd43e5680224d7fcdec33c4e8acb941c13
yiyeah <68832436+yiyeah@users.noreply.github.com>
2021-10-06 15:20:16 +0800

    Add files via upload

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+    }
+}
