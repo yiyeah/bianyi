d614d6b8f68601269ef0c85e1cba5be88c225257
yiyeah <68832436+yiyeah@users.noreply.github.com>
2021-11-30 20:19:29 +0800

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextLine()){
+            String out=in.nextLine();
+            System.out.println(out);
+        }
+        return;
+    }
+}
